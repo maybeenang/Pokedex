609294c1b117b5a31b2d5e4c23bfa8931066405c
maybeenang <muhammad.120140194@student.itera.ac.id>
2022-11-02 01:14:23 +0700

    poke

---
+++ script.js
@@ -0,0 +1,52 @@
+let pokemon = [];
+
+const getAllPokemon = async () => {
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon");
+  const data = await res.json();
+
+  const getPokemon = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  };
+
+  const pokemonData = await Promise.all(
+    data.results.map(async (pokemon) => {
+      const pokemonRecord = await getPokemon(pokemon.url);
+      return pokemonRecord;
+    })
+  );
+
+  pokemon = pokemonData;
+};
+
+const pokemonContainer = document.querySelector(".pokemonContainer");
+
+const displayPokemon = () => {
+  const pokemonHTMLString = pokemon.map((pokeman) => {
+    return `
+        <div class="pokemon">
+        <div class="pokemonID">
+          <p>${pokeman.order}</p>
+        </div>
+        <div class="pokemonImg">
+          <img
+            src="${pokeman.sprites.other.dream_world.front_default}"
+            alt="${pokeman.name}"
+          />
+        </div>
+        <div class="pokemonDesc">
+          <p class="pokemonName">${pokeman.name}</p>
+          <div class="pokemonTypeContainer">
+            <p class="pokemonType">weight : ${pokeman.weight}</p>
+          </div>
+        </div>
+      </div>
+        `;
+  });
+  pokemonContainer.innerHTML = pokemonHTMLString.join("");
+};
+
+getAllPokemon().then(() => {
+  displayPokemon();
+});
